7e316b1e6bfcdcbcb6496dedabd65116122fa40f
Zachary Farrell <zfarrell0507@gmail.com>
2022-07-25 18:43:39 -0400

    commit tree f9f10dab0b103bf484ba7705516a9a70505f0303

--- main.js
+++ main.js
@@ -3,9 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const allHearts = document.querySelectorAll(".like-glyph");
 
+function likeAction(event) {
+  const heart = event.target;
+  mimicServerCall()
+    .then(function(){
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } 
+      else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+    .catch(function(error) {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+}
 
-
+for(const glyph of allHearts) {
+  glyph.addEventListener('click', likeAction);
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
